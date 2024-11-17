c5e15db16a169b827154ffc8a463a28ffccbad2a
정성욱 <callmeson1@naver.com>
2024-11-17 20:55:06 +0900

    IO Count, send/recv Post 함수 추가

--- 20241113 IOCP/main.cpp
+++ 20241113 IOCP/main.cpp
@@ -34,24 +34,25 @@ public:
 
     OVERLAPPED overlappedRecv;
     OVERLAPPED overlappedSend;
-};
 
-class CPlayer
-{
-public:
-    CPlayer() {}
-    ~CPlayer() {}
+    UINT32 id;
 
-public:
-    UINT32 m_playerID;
+    UINT32 IOCount;
 };
 
 SOCKET g_listenSocket; 
-HANDLE g_hIOCP;
+HANDLE g_hIOCP; 
+UINT32 g_id = 0;
+
+// key로 id를 받아 관리되는 세션 맵
+std::unordered_map<UINT32, CSession*> g_clients;
 
 // 작업자 스레드 함수
-unsigned int WINAPI WorkerThread(void* arg);
+unsigned int WINAPI WorkerThread(void* pArg);
 unsigned int WINAPI AcceptThread(void* pArg);
+
+void SendPost(CSession* pSession);
+void RecvPost(CSession* pSession);
 
 int main(void)
 {
@@ -84,9 +85,9 @@ int main(void)
     }
 
 
-    // recvbuf 크기 설정 (송신 버퍼의 크기를 0으로 설정해 중간 단계 버퍼링 없이 메모리에 직접 direct I/O를 유도)
+    // sendbuf 크기 설정 (송신 버퍼의 크기를 0으로 설정해 중간 단계 버퍼링 없이 메모리에 직접 direct I/O를 유도)
     int recvBufSize = 0;
-    if (setsockopt(g_listenSocket, SOL_SOCKET, SO_RCVBUF, (const char*)&recvBufSize, sizeof(recvBufSize)) == SOCKET_ERROR) {
+    if (setsockopt(g_listenSocket, SOL_SOCKET, SO_SNDBUF, (const char*)&recvBufSize, sizeof(recvBufSize)) == SOCKET_ERROR) {
         std::cerr << "setsockopt failed with error: " << WSAGetLastError() << std::endl;
         closesocket(g_listenSocket);
         WSACleanup();
@@ -163,26 +164,28 @@ int main(void)
 
 
 // 작업자 스레드 함수
-unsigned int WINAPI WorkerThread(LPVOID arg)
+unsigned int WINAPI WorkerThread(void* pArg)
 {
     int retval;
+    int error;
+
+    // 비동기 입출력 완료 기다리기
+    DWORD transferredDataLen;
+
+    CSession* pSession;
+
+    // GQCS의 반환값은 성공하면 TRUE, 아니면 FALSE.
+    OVERLAPPED* pOverlapped;
 
     while (1) {
-        // 비동기 입출력 완료 기다리기
-        DWORD transferredDataLen{};
-
-        CSession* pSession = nullptr;
-
-        // GQCS의 반환값은 성공하면 TRUE, 아니면 FALSE.
-        OVERLAPPED* pOverlapped;
+        transferredDataLen = 0;
         memset(&pOverlapped, 0, sizeof(pOverlapped));
 
         // GQCS로 IOCP 핸들로 온 완료통지를 가져옴.
         // 반환값으로 몇 바이트를 받았는지, 어떤 키인지, 어떤 오버랩 구조체를 사용했는지를 알려준다.
         retval = GetQueuedCompletionStatus(g_hIOCP, &transferredDataLen, (PULONG_PTR)&pSession, &pOverlapped, INFINITE);
 
-        int wsaError = WSAGetLastError();
-        int Error = GetLastError();
+        error = WSAGetLastError();
 
         // 예외 처리 - GQCS가 실패했거나, cbTransferred이 0인 경우(rst가 온 경우)
         if (retval == FALSE || transferredDataLen == 0 )
@@ -190,12 +193,32 @@ unsigned int WINAPI WorkerThread(LPVOID arg)
             // PQCS로 넣은 완료 통지값(0, 0, NULL)이 온 경우. 즉, 내가 먼저 끊은 경우
             if (pSession == 0 && pOverlapped == nullptr)
             {
-                // while문을 빠져나간다.
+                // while문을 빠져나간다. 이러면 완료통지가 남아 있어도 스레드는 종료된다.
+               
+                // 완료통지 처리 이후 IO Count 1 감소
+                pSession->IOCount--;
+
+                // IO Count가 0인 경우 
+                if (pSession->IOCount == 0)
+                {
+                    // 소켓 닫기
+                    closesocket(pSession->sock);
+
+                    // 세션 삭제
+                    delete pSession;
+                }
+                else
+                {
+                    // 이 부분은 확실히 에러이다. 삭제되어야하는 상황에서 IOCount가 0이 아니니깐. 
+                    // 완료통지를 다 처리 안했는데 종료 통지가 온 경우, 이럴 수 있다. 
+                    // 그래도 이 부분을 보기 위해서 debugbreak를 남김.
+                    DebugBreak();
+                }
                 break;
             }
 
             // 상대방에서 먼저 끊어서 이쪽에서 확인해보니 네트워크가 더 이상 없는 경우
-            if (wsaError == ERROR_NETNAME_DELETED)
+            if (error == ERROR_NETNAME_DELETED)
             {
                 continue;
             }
@@ -206,6 +229,7 @@ unsigned int WINAPI WorkerThread(LPVOID arg)
 
         // recv 완료 통지가 온 경우
         else if (pOverlapped == &pSession->overlappedRecv) {
+
             // 링버퍼를 사용하기에 버퍼에 데이터만 존재하고, 사이즈는 증가하지 않았다. 고로 사이즈를 증가시킨다.
             pSession->recvQ.MoveRear(transferredDataLen);
 
@@ -215,42 +239,30 @@ unsigned int WINAPI WorkerThread(LPVOID arg)
 
             pSession->sendQ.Enqueue(temp, transferredDataLen);
 
-            WSABUF wsaBuf[2];
-            wsaBuf[0].buf = pSession->sendQ.GetFrontBufferPtr();
-            wsaBuf[0].len = pSession->sendQ.DirectDequeueSize();
+            // send 처리
+            SendPost(pSession);
 
-            wsaBuf[1].buf = pSession->sendQ.GetBufferPtr();
-
-            // 보내려는 데이터의 길이가 sendQ에서 뺄 수 있는 데이터의 길이보다 크다면
-            if (transferredDataLen > (DWORD)pSession->sendQ.DirectDequeueSize())
-            {
-                wsaBuf[1].len = transferredDataLen - wsaBuf[0].len;
-            }
-            // 보내려는 데이터의 길이가 sendQ에서 뺄 수 있는 데이터의 길이와 비교했을 때 작거나 같다면
-            else
-            {
-                wsaBuf[1].len = 0;
-            }
-
-            DWORD flags{};
-            WSASend(pSession->sock, wsaBuf, 2, NULL, flags, &pSession->overlappedSend, NULL);
-
-
-
-            // 다시 recv를 걸어둬서 비동기적으로 recv가 처리되도록 함
-            wsaBuf[0].buf = pSession->recvQ.GetFrontBufferPtr();
-            wsaBuf[0].len = pSession->recvQ.DirectEnqueueSize();
-            wsaBuf[1].buf = pSession->recvQ.GetBufferPtr();
-            wsaBuf[1].len = pSession->recvQ.GetBufferCapacity() - pSession->recvQ.DirectEnqueueSize();
-
-            flags = 0;
-            WSARecv(pSession->sock, wsaBuf, 2, NULL, &flags, &pSession->overlappedRecv, NULL);
+            // recv 처리
+            RecvPost(pSession);
         }
 
         // send 완료 통지가 온 경우
         else if (pOverlapped == &pSession->overlappedSend) {
             // 보내는 것을 완료 했으므로 sendQ에 있는 데이터를 제거
             pSession->sendQ.MoveFront(transferredDataLen);
+        }
+
+        // 완료통지 처리 이후 IO Count 1 감소
+        pSession->IOCount--;
+
+        // IO Count가 0인 경우 
+        if (pSession->IOCount == 0)
+        {
+            // 소켓 닫기
+            closesocket(pSession->sock);
+
+            // 세션 삭제
+            delete pSession;
         }
     }
 
@@ -263,10 +275,12 @@ unsigned int WINAPI AcceptThread(void* pArg)
     SOCKET client_sock;
     SOCKADDR_IN clientaddr;
 
+    int addrlen;
+
     while (true)
     {
         // Accept
-        int addrlen = sizeof(clientaddr);
+        addrlen = sizeof(clientaddr);
         client_sock = accept(g_listenSocket, (SOCKADDR*)&clientaddr, &addrlen);
         if (client_sock == INVALID_SOCKET) {
             std::cout << "accept()\n";
@@ -300,19 +314,90 @@ unsigned int WINAPI AcceptThread(void* pArg)
         // client 소켓 연결
         pSession->sock = client_sock;
 
+        // client ID 부여
+        pSession->id = g_id;
+        g_id++;
+
+        // IO Count 부야
+        pSession->IOCount = 0;
+
+
 
         // 소켓과 입출력 완료 포트 연결 - 새로 연견될 소켓을 key를 해당 소켓과 연결된 세션 구조체의 포인터로 하여 IOCP와 연결한다.
         CreateIoCompletionPort((HANDLE)client_sock, g_hIOCP, (ULONG_PTR)pSession, 0);
 
-        DWORD flags{};
 
-        WSABUF recvBuf;
-        recvBuf.buf = pSession->recvQ.GetFrontBufferPtr();
-        recvBuf.len = pSession->recvQ.GetBufferCapacity();
-
-        // 비동기 recv 처리를 위해 먼저 recv를 걸어둠. 받은 데이터는 wsaBuf에 등록한 메모리에 채워질 예정.
-        WSARecv(pSession->sock, &recvBuf, 1, NULL, &flags, &pSession->overlappedRecv, NULL);
+        // recv 처리
+        RecvPost(pSession);
     }
 
     return 0;
 }
+
+
+void SendPost(CSession* pSession)
+{
+    int retval;
+    int error;
+
+    WSABUF wsaBuf[2];
+    wsaBuf[0].buf = pSession->sendQ.GetFrontBufferPtr();
+    wsaBuf[0].len = pSession->sendQ.DirectDequeueSize();
+
+    wsaBuf[1].buf = pSession->sendQ.GetBufferPtr();
+
+    int transferredDataLen = pSession->sendQ.GetUseSize();
+
+    // 보내려는 데이터의 길이가 sendQ에서 뺄 수 있는 데이터의 길이보다 크다면
+    if (transferredDataLen > pSession->sendQ.DirectDequeueSize())
+    {
+        wsaBuf[1].len = transferredDataLen - wsaBuf[0].len;
+    }
+    // 보내려는 데이터의 길이가 sendQ에서 뺄 수 있는 데이터의 길이와 비교했을 때 작거나 같다면
+    else
+    {
+        wsaBuf[1].len = 0;
+    }
+
+    DWORD flags{};
+    pSession->IOCount++;
+    retval = WSASend(pSession->sock, wsaBuf, 2, NULL, flags, &pSession->overlappedSend, NULL);
+
+    error = WSAGetLastError();
+    if (retval == SOCKET_ERROR)
+    {
+        if (error == ERROR_IO_PENDING)
+            ;
+        else
+            DebugBreak();
+    }
+}
+
+void RecvPost(CSession* pSession)
+{
+    int retVal;
+    int error;
+
+    DWORD flags{};
+
+    WSABUF recvBuf[2];
+    recvBuf[0].buf = pSession->recvQ.GetFrontBufferPtr();
+    recvBuf[0].len = pSession->recvQ.DirectEnqueueSize();
+    recvBuf[1].buf = pSession->recvQ.GetBufferPtr();
+    recvBuf[1].len = pSession->recvQ.GetBufferCapacity() - pSession->recvQ.DirectEnqueueSize();
+   
+    pSession->IOCount++;
+    // 비동기 recv 처리를 위해 먼저 recv를 걸어둠. 받은 데이터는 wsaBuf에 등록한 메모리에 채워질 예정.
+    retVal = WSARecv(pSession->sock, recvBuf, 2, NULL, &flags, &pSession->overlappedRecv, NULL);
+
+    error = WSAGetLastError();
+
+    if (retVal == SOCKET_ERROR)
+    {
+        if (error == ERROR_IO_PENDING)
+            return;
+
+        DebugBreak();
+    }
+}
+
